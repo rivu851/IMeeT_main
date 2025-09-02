36de5fb4bb3f997176f5de3d239cf8466a518dcf
Rivu Chattopadhyay <rivu851@gmail.com>
2025-09-02 15:55:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "@fontsource/anton"; // Loads Anton
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+ <StrictMode>
+  <Auth0Provider
+    domain="dev-jgsawtvpf2vqmlx7.us.auth0.com"
+    clientId="pTim8FLK8snpTHsXPfJkoUStnjS38M3Y"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    cacheLocation="localstorage"
+  >
     <App />
-  </StrictMode>,
+  </Auth0Provider>
+</StrictMode>
 )
